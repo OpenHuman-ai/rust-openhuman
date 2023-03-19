f5bc8b4ab39f12d5176a88e4d1ff9672a5f26cf7
hmthanh <hmthanhgm@gmail.com>
2023-03-19 17:42:02 +0700

    update all

--- src/main.rs
+++ src/main.rs
@@ -23,25 +23,25 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-#[cfg(test)]
-mod tests {
-    use actix_web::{body::to_bytes, dev::Service, http, test, web, App, Error};
+// #[cfg(test)]
+// mod tests {
+//     use actix_web::{body::to_bytes, dev::Service, http, test, web, App, Error};
 
-    use super::*;
+//     use super::*;
 
-    #[actix_web::test]
-    async fn test_index() -> Result<(), Error> {
-        let app = App::new().route("/", web::get().to(index));
-        let app = test::init_service(app).await;
+//     #[actix_web::test]
+//     async fn test_index() -> Result<(), Error> {
+//         let app = App::new().route("/", web::get().to(index));
+//         let app = test::init_service(app).await;
 
-        let req = test::TestRequest::get().uri("/").to_request();
-        let resp = app.call(req).await?;
+//         let req = test::TestRequest::get().uri("/").to_request();
+//         let resp = app.call(req).await?;
 
-        assert_eq!(resp.status(), http::StatusCode::OK);
+//         assert_eq!(resp.status(), http::StatusCode::OK);
 
-        let response_body = resp.into_body();
-        assert_eq!(to_bytes(response_body).await?, r##"Hello world!"##);
+//         let response_body = resp.into_body();
+//         assert_eq!(to_bytes(response_body).await?, r##"Hello world!"##);
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
